4925ca9a51a532c10a5afcd33007673f0ac7d04f
Şafak Ferhat Kaya <safakferhatkaya@gmail.com>
2021-09-21 18:34:19 +0300

    holidays iterated over country code and listed
    %todo
    filter spec according to countries
    "about me" page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,37 +1,13 @@
 class HomeController < ApplicationController
   def index
-    require 'ruby-holidayapi'
-    require 'json'
-    key = '5070eaee-9c15-4036-a876-265cb4d17ec8'
-    hapi = HolidayAPI::V1.new(key)
-    @holidays_output = hapi.holidays({
-    'country': 'TR',
-    'year': '2020',
-    })
-#    @holidays_hash = @holidays_output["holidays"]
-# =>  @holidays_name = @holidays_hash["name"]s
-
-     if @holidays_output.empty?
-        @final_output_holiday_name = "Error"
-     else
-      if(record_flag!=1)
-        i=0
-        while i<25
-           @final_output_holiday_name = @holidays_output["holidays"][i]["name"]
-           @final_output_holiday_date = @holidays_output["holidays"][i]["date"]
-           @final_output_holiday_country = @holidays_output["holidays"][i]["country"]
-           @test_ = Holiday.new({ "holiday_name" => "#{@final_output_holiday_name}", 
-          "date" => "#{@final_output_holiday_date}", 
-          "country" => "#{@final_output_holiday_country}"})
-           @test_.save
-           i+=1
-        end#while's end
-        
-      end
-     end#if's end
   end
   def about
   end
   def countries
+    country_list = []
+    country_list = Array.new
+    require 'i18n_data'
+
+  
   end
 end
--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,3 +1,36 @@
 class Holiday < ApplicationRecord
-
-end
+    require 'ruby-holidayapi'
+    require 'json'
+    require 'i18n_data'
+    I18nData.countries.flatten
+    @country_and_names = I18nData.countries.flatten
+    #o.txt buraya
+    i=2
+    k=0
+    key = '5070eaee-9c15-4036-a876-265cb4d17ec8'
+    while i<(@country_and_names).length
+        hapi = HolidayAPI::V1.new(key)
+        
+        @holidays_output = hapi.holidays({
+       'country': "#{@country_and_names[i]}",
+       'year': '2020',
+        })
+        i+=2
+        if @holidays_output["status"]!=200
+            @final_output_holiday_name = "Error"
+        else
+        k=0
+        while k<(@holidays_output["holidays"]).size
+           @final_output_holiday_name = @holidays_output["holidays"][k]["name"]
+           @final_output_holiday_date = @holidays_output["holidays"][k]["date"]
+           @final_output_holiday_country = @holidays_output["holidays"][k]["country"]
+           @test_ = Holiday.new({ "holiday_name" => "#{@final_output_holiday_name}", 
+          "date" => "#{@final_output_holiday_date}", 
+          "country" => "#{@final_output_holiday_country}"})
+           @test_.save
+           k+=1
+            
+            end#little while's end
+        end
+    end#big while's end
+end#class's end
